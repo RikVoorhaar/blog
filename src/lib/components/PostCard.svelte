4ca1942d35cdf9f6c610f05639236486593757b6
Rik Voorhaar <rik.voorhaar@gmail.com>
2023-11-19 16:25:06 +0100

    background image

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -7,30 +7,32 @@
 	let teaser_url: string = `/blog/teasers/original/${post.teaser}`;
 </script>
 
-<div class="p-4 m-0 relative group w-[320px]">
-	<div class="absolute inset-0 bg-gradient-to-r from-main-100 to-secondary-100 dark:from-blue-800 dark:to-blue-900 rounded-lg" />
+<div class="p-4 m-0 relative group w-[320px] shadow-lg">
+	<div class="absolute inset-0 bg-gradient-to-r from-main-100 to-secondary-100 dark:from-main-900 dark:to-secondary-900 rounded-lg" />
 	<div
-		class="absolute inset-0 bg-gradient-to-r from-main-300 to-secondary-300 
-		dark:from-blue-900 dark:to-blue-950
+		class="absolute inset-0 bg-gradient-to-r from-main-200 to-secondary-200 
+		dark:from-secondary-800 dark:to-main-800
 		opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-lg"
 	/>
 	<div class="relative z-10">
 		<div class="content-center py-2 pb-4">
-			<img
-				src={teaser_url}
-				alt="teaser"
-				class="m-auto rounded-lg bg-white max-h-52 object-contain"
-			/>
+			<a href={post.slug}>
+				<img
+					src={teaser_url}
+					alt="teaser"
+					class="m-auto rounded-lg bg-white max-h-52 object-contain"
+				/>
+			</a>
 		</div>
-		<a href={post.slug} class="text-lg font-bold hover:text-blue-500 transition-colors"
+		<a href={post.slug} class="text-lg font-bold leading-5 text-main-600 dark:text-main-300 hover:text-main-800 dark:hover:text-main-100 transition-colors tracking-wide"
 			>{post.title}</a
 		>
 		<p class="text-sm">{formatDate(post.date)}</p>
-		<p class="px-2 my-1 border-l-2 border-l-black dark:border-l-white">
+		<p class="px-2 my-1 border-l-2 border-l-main-800 dark:border-l-main-300 leading-5 font-light">
 			{post.excerpt}
 			<a
 				href={post.slug}
-				class="dark:text-blue-300 dark:hover:text-blue-100 text-blue-600 hover:text-blue-800"
+				class="dark:text-main-300 dark:hover:text-main-100 text-main-600 hover:text-main-400 transition-colors"
 				>read more ...</a
 			>
 		</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 </script>
 
 <div class="flex flex-auto grow gap-4 flex-wrap justify-center sm:p-8 xl:px-16">
-<!-- grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 "> -->
 	{#each data.posts as post}
 		<PostCard post={post} />
 	{/each}
