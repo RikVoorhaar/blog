349cd1acb932fc9dd689604b95c6f9028cf025cf
Rik Voorhaar <rik.voorhaar@gmail.com>
2023-11-27 22:33:26 +0100

    Start cv

---
+++ src/lib/components/cv/Experience.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let data: {
+		date_start: string;
+		date_end: string;
+		position: string;
+		employer: string;
+		image: string;
+	};
+</script>
+
+<div class="flex">
+	<div class="w-36 mr-6 flex-shrink-0">
+		<img src={data.image} alt={data.employer} class="w-full h-auto" />
+	</div>
+	<div class="flex-grow">
+		<h3>
+			{@html data.employer}<span class="text-black dark:text-white ml-4">{data.position}</span>
+		</h3>
+		<p class="italic">{data.date_start}&mdash;{data.date_end}</p>
+		<slot />
+	</div>
+</div>
--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,7 +1,61 @@
 <script lang="ts">
-    import Container from "$lib/components/Container.svelte";
+	import Container from '$lib/components/Container.svelte';
+	import Experience from '$lib/components/cv/Experience.svelte';
 </script>
 
 <Container>
-    This is actually a test!
+	<Experience
+		data={{
+			date_start: 'January 2023',
+			date_end: '',
+			position: 'Software Developer',
+			employer: 'Grazper Technologies',
+			image: '/cv/grazper_circle.svg'
+		}}
+	>
+		<p>
+			Developed an augmented reality system using real-time 3d pose estimation to display holograms
+			on a screen from the correct 3d perspective of a person standing next to it. This is a full
+			stack project, with frontends for calibration procedures and displaying holograms, and
+			backends for interfacing with hardware and numerical algorithms.
+		</p>
+
+		<p>
+			Developed a 'service broker' system for monitoring and launching all our services, and to
+			allow services to find each other. This system makes it much easier to start up and develop
+			product demos, as well as providing a convenient place to monitor running servces. The backend
+			uses a combination of our own network stack, together with FastAPI, and the frontend is
+			developed in Svelte with Tailwind CSS.
+		</p>
+
+		<p>
+			Maintained and upgraded the Jenkins CI/CD pipeline, improving the workflow for all our Python
+			projects.
+		</p>
+	</Experience>
+	<Experience
+		data={{
+			date_start: 'March 2018',
+			date_end: 'December 2022',
+			position: 'Doctoral Candidate',
+			employer: 'University of Geneva',
+			image: '/cv/fac_sciences_pant.svg'
+		}}
+	>
+		<p>
+			Performed research in applied mathematics after first studying pure
+			mathematics for 2 years. Focussed on advanced numerical optimization
+			algorithms, particularly for tensors and their applications to machine
+			learning. Developed 3 high-quality numerical software libraries in Python
+			and contributed to 2 open-source projects as part of several research
+			projects resulting in 4 manuscripts.
+		</p>
+
+		<p>
+			Taught 3 courses per year as an assistant, receiving consistently positive
+			feedback from students for clear solutions and lectures. Designed Python
+			programming homework using Jupyter and unit tests for 5 courses
+			well-received by both students and teaching staff.
+		</p>
+	</Experience>
 </Container>
